2d1066a88c3dc78e58d107faaf34f7a3bf446d2e
mitkoarsov <mitko.arsov.dev@gmail.com>
2025-10-26 20:08:12 +0100

    config: including .env.development and .env.production files

--- vite.config.ts
+++ vite.config.ts
@@ -1,59 +1,46 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-const isProd: boolean = process.env.REACT_APP_IS_PROD === "true";
-const parentUrl: string = isProd
-  ? "https://mitkoarsov.github.io/microfrontends-parent/assets/remoteEntry.js"
-  : "http://localhost:5173/assets/remoteEntry.js";
+export default defineConfig(({ mode }) => {
+  const env: Record<string, string> = loadEnv(mode, process.cwd(), "");
+  const isProd: boolean = env.REACT_APP_IS_PROD === "true";
+  const parentUrl: string = isProd
+    ? "https://mitkoarsov.github.io/microfrontends-parent/assets/remoteEntry.js"
+    : "http://localhost:5174/assets/remoteEntry.js";
 
-export default defineConfig({
-  plugins: [
-    federation({
-      name: "app1",
-      filename: "remoteEntry.js",
-      exposes: { "./App": "./src/App.tsx" },
-      remotes: {
-        shell: parentUrl,
-      },
-      shared: [
-        "react",
-        "react-dom",
-        "react-redux",
-        "@reduxjs/toolkit",
-        "use-sync-external-store",
-        "@mui/material",
-        "@emotion/react",
-        "@emotion/styled",
-      ],
-    }),
-    react(),
-  ],
-  // server: { port: 5174, strictPort: true, cors: true },
-  // preview: {
-  //   port: 5174,
-  //   strictPort: true,
-  //   headers: { "Access-Control-Allow-Origin": "*" },
-  // },
-  // optimizeDeps: {
-  //   exclude: ["shell", "shell/global"],
-  //   include: [
-  //     "react",
-  //     "react-dom",
-  //     "react-redux",
-  //     "@reduxjs/toolkit",
-  //     "use-sync-external-store",
-  //     "use-sync-external-store/shim",
-  //     "use-sync-external-store/shim/with-selector",
-  //     "use-sync-external-store/with-selector",
-  //   ],
-  // },
-  resolve: {
-    dedupe: ["react", "react-dom", "react-redux", "use-sync-external-store"],
-  },
-  // build: {
-  //   target: "esnext",
-  //   modulePreload: false,
-  //   chunkSizeWarningLimit: 1500,
-  // },
+  console.log("isProd:", isProd);
+
+  return {
+    plugins: [
+      federation({
+        name: "app1",
+        filename: "remoteEntry.js",
+        exposes: { "./App": "./src/App.tsx" },
+        remotes: {
+          shell: parentUrl,
+        },
+        shared: [
+          "react",
+          "react-dom",
+          "react-redux",
+          "@reduxjs/toolkit",
+          "use-sync-external-store",
+          "@mui/material",
+          "@emotion/react",
+          "@emotion/styled",
+        ],
+      }),
+      react(),
+    ],
+    server: {
+      port: 5174,
+      strictPort: true,
+      cors: true,
+    },
+
+    resolve: {
+      dedupe: ["react", "react-dom", "react-redux", "use-sync-external-store"],
+    },
+  };
 });
